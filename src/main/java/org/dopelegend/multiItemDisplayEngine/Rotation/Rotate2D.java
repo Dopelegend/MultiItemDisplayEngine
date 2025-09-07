148321c1fcfa0086ed6977e76b204824e4184451
Dopy <lassetheilgaardandersen@gmail.com>
2025-09-07 17:59:07 +0200

    A plugin made primarily for custom vehicles and entities using multiple ItemDisplays for models

--- src/main/java/org/dopelegend/multiItemDisplayEngine/Rotation/Rotate2D.java
+++ src/main/java/org/dopelegend/multiItemDisplayEngine/Rotation/Rotate2D.java
@@ -1,5 +1,7 @@
 package org.dopelegend.multiItemDisplayEngine.Rotation;
 
 public class Rotate2D {
-
+    public static void rotate(double angle){
+        double radians = Math.toRadians(angle);
+    }
 }
