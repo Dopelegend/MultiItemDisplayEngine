0ff75d11db215a65e0a10a929ff14d499f99d166
Dopy <lassetheilgaardandersen@gmail.com>
2025-09-08 20:56:23 +0200

    First prototype of the Rotate2D func

--- src/main/java/org/dopelegend/multiItemDisplayEngine/Rotation/Rotate2D.java
+++ src/main/java/org/dopelegend/multiItemDisplayEngine/Rotation/Rotate2D.java
@@ -1,7 +1,20 @@
 package org.dopelegend.multiItemDisplayEngine.Rotation;
 
+
+import org.bukkit.Location;
+
 public class Rotate2D {
-    public static void rotate(double angle){
-        double radians = Math.toRadians(angle);
+    /**
+     *
+     * @param angle How many radians the object should be rotated around the center
+     * @param centerOfRotation The point the object should be rotated around
+     * @param object The object that is rotated
+     */
+    public static void rotate(double angle, Location centerOfRotation, Location object) {
+        double radius = centerOfRotation.distance(object);
+        double totalAngle = angle+Math.asin(radius/(object.getZ()-centerOfRotation.getZ()));
+        object.setX(centerOfRotation.getX()+Math.cos(totalAngle)*radius);
+        object.setZ(centerOfRotation.getZ()+Math.sin(totalAngle)*radius);
+        object.setYaw((float) (object.getYaw()+Math.toDegrees(angle)));
     }
 }
